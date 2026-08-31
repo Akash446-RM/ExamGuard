ce289205896db6bd7e1c06c420b80fe1c32e2842
Akash446-RM <akashrmakashrm70@gmail.com>
2026-08-31 18:54:16 +0530

    creat signup and login

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect
 from database import init_db, get_db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 app = Flask(__name__)
+app.secret_key = "exam_guard_key"  # Required for session management
+
 
 # Initialize database
 init_db()
@@ -20,6 +24,10 @@ def register():
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
+        hashed_password = generate_password_hash(password)
+        print(f"Name: {name}, Email: {email}, Password: {hashed_password}")
+
+
 
         connection = get_db()
 
@@ -29,46 +37,61 @@ def register():
             (name, email, password)
             VALUES (?, ?, ?)
             """,
-            (name, email, password)
+            (name, email, hashed_password)
         )
 
         connection.commit()
         connection.close()
 
-        return "Registration successful"
-
+        #return "Registration successful"
+        return redirect("/login")
+        
     return render_template("register.html")
 
 
+# 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+
     if request.method == "POST":
+
         email = request.form["email"]
         password = request.form["password"]
 
         connection = get_db()
-        cursor = connection.cursor()
 
-        cursor.execute(
-            """
-            SELECT * FROM candidates
-            WHERE email = ? AND password = ?
-            """,
-            (email, password)
-        )
+        try:
+            candidate = connection.execute("""
+                SELECT *
+                FROM candidates
+                WHERE email = ? 
+            """, (email,)
+            ).fetchone()
 
-        candidate = cursor.fetchone()
-        connection.close()
+            if candidate and check_password_hash(candidate["password"], password):
+         
+                session["candidate_id"] = candidate["id"]
+                return redirect("/dashboard")
 
-        if candidate:
-            return "Login successful"
-        else:
-            return "Invalid credentials"
+            return "Invalid email or password"
+
+        finally:
+            connection.close()
 
     return render_template("login.html")
 
+@app.route("/dashboard")
+def dashboard():
+    if 'candidate_id' not in session:
+        return "Please log in first"
+    
+    return render_template("dashboard.html")
 
 
+@app.route("/logout")
+def logout():
+    session.pop('candidate_id', None)
+    return "Logged out successfully"
 
 
 
